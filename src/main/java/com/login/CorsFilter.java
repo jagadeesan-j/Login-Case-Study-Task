ae0bbd37c90ca502b7830348573abf7eda5974b0
Jagadeesan J <65103993+jagadeesan-j@users.noreply.github.com>
2023-04-28 07:17:00 +0530

    Initial commit

---
+++ src/main/java/com/login/CorsFilter.java
@@ -0,0 +1,30 @@
+package com.login;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+		httpResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+		httpResponse.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+}
